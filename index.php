7e4576b7ee39415ea96e1d155864192d99545cae
adreew <premier22@yandex.ru>
2016-05-27 17:17:06 +0300

    --

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
     echo 'this is GIT TEST<br>';
 	echo '2th exeding master branch3<br>';
+	echo '1<br>';
+	echo '2<br>';
+	echo '3<br>';
+	echo '4<br>';
+	echo '5<br>';
+	echo '6<br>';
+	echo '7<br>';
+	echo '8<br>';
 ?>
